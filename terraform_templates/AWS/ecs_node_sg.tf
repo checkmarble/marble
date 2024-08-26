e36ae185a82db432800b2ba82a63fede6b29b232
nickykln <kleinnic@gmail.com>
2024-08-27 00:35:34 +0200

    Change to Bridge Mode

--- terraform_templates/AWS/ecs_node_sg.tf
+++ terraform_templates/AWS/ecs_node_sg.tf
@@ -3,7 +3,12 @@
 resource "aws_security_group" "ecs_node_sg" {
   name_prefix = "marble-ecs-node-sg-"
   vpc_id      = aws_vpc.main.id
-
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    security_groups = [ aws_security_group.http.id ]
+  }
   egress {
     from_port   = 0
     to_port     = 65535
