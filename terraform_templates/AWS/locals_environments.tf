d6f36c9359c1577fe63a400944845b934b36fcd1
nickykln <kleinnic@gmail.com>
2024-08-29 11:21:43 +0200

    Remove PP Custom variables

--- terraform_templates/AWS/locals_environments.tf
+++ terraform_templates/AWS/locals_environments.tf
@@ -2,29 +2,29 @@ locals {
 
   environments = {
 
-    default = {
+    production = {
       # TO CONFIGURE
       firebase : {
         // Copy the JSON Configuration File from Firebase Console
-        apiKey : "AIzaSyAtbEKtaTrIRdmdxs98GPMJBSwTxCkV_gc",
-        authDomain : "pixpaymarble.firebaseapp.com",
-        projectId : "pixpaymarble",
-        storageBucket : "pixpaymarble.appspot.com",
-        messagingSenderId : "886300766582",
-        appId : "1:886300766582:web:da1111d386d37e85e74a57"
+        apiKey : "...",
+        authDomain : "...",
+        projectId : "...",
+        storageBucket : "...",
+        messagingSenderId : "...",
+        appId : "..."
       }
       
       licence_key = ""
 
       session : {
-        secret  = "JeAT8VCKjBs7gVrFY23PG7aSMPqcvNFE", // Change It
+        secret  = "...", // Change It
         max_age = "43200"
       }
 
       org : {
-        global = "devs@pixpay.fr", // Gloabl Admin Email Address
-        name   = "Pixpay", // Organization name
-        admin  = "nicolas@pixpay.fr" // Organization Admin Email Address
+        global = "...@...", // Gloabl Admin Email Address
+        name   = "...", // Organization name
+        admin  = "...@..." // Organization Admin Email Address
       }
 
       segment_write_key = {
@@ -34,25 +34,25 @@ locals {
 
       sentry = {
         frontend = {
-          dsn = "https://35baab2abd1e46e4d6c28b4963766727@o226978.ingest.us.sentry.io/4507621335367680",
+          dsn = "...",
           env = "prod"
         }
         backend  = {
-          dsn  =  "https://e2e1767e50571d033eb55a4bdf5f26d9@o226978.ingest.us.sentry.io/4507856676126720",
+          dsn  =  "...",
           env   =  "prod"
         }
       }
 
       frontend = {
         image  = "europe-west1-docker.pkg.dev/marble-infra/marble/marble-frontend:latest"
-        domain = "marble.pixpay.app" // Your Application Domain
-        url    = "https://marble.pixpay.app" // Your Application URL
+        domain = "..." // Your Application Domain (ex. app.xxx.xxx)
+        url    = "..." // Your Application URL (ex. https://marble-app.xxx.xxx)
       }
 
       backend = {
         image              = "europe-west1-docker.pkg.dev/marble-infra/marble/marble-backend:latest"
-        domain             = "marble-api.pixpay.app" // Your API Domain
-        url                = "https://marble-api.pixpay.app" // Your API URL
+        domain             = "..." // Your API Domain (ex. api.xxx.xxx)
+        url                = "..." // Your API URL (ex. https://marble-api.xxx.xxx)
         max_instance_count = 3
       }
 
