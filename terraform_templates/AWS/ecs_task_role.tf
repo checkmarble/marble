0151c5ccc798ff1b6b9da87a28e816c6c128915d
jimmybastos <eujimmbastos@gmail.com>
2025-07-31 15:31:39 -0300

    feat: add s3 access to ecs task policy

--- terraform_templates/AWS/ecs_task_role.tf
+++ terraform_templates/AWS/ecs_task_role.tf
@@ -26,3 +26,29 @@ resource "aws_iam_role_policy_attachment" "ecs_exec_role_policy" {
   role       = aws_iam_role.ecs_exec_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_role_policy" "ecs_task_s3_policy" {
+  name = "marble-ecs-task-s3-policy"
+  role = aws_iam_role.ecs_task_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.ingestion.arn,
+          "${aws_s3_bucket.ingestion.arn}/*"
+        ]
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket.ingestion]
+}
